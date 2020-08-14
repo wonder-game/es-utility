fa6452f3030b5896a150749985c703973282f0d9
lamson <171004297@qq.com>
2020-08-14 14:28:30 +0800

    修改注释里的版本

--- src/Traits/LamController.php
+++ src/Traits/LamController.php
@@ -3,7 +3,7 @@
  * 通用控制器组件
  *
  * @author 林坤源
- * @version 7.0.0 最后修改时间 2020年08月14日
+ * @version 1.0.0 最后修改时间 2020年08月14日
  */
 namespace Traits;
 
--- src/Traits/LamModel.php
+++ src/Traits/LamModel.php
@@ -3,11 +3,11 @@
  * 通用模型组件
  *
  * @author 林坤源
- * @version 7.0.0 最后修改时间 2020年08月14日
+ * @version 1.0.0 最后修改时间 2020年08月14日
  */
 namespace Traits;
 
 trait LamModel
 {
-	
+
 }
