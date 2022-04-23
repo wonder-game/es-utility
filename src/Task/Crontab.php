72b0c5c107162ffe22be0fbd8597dcae9ef2b972
Joyboo <zhoubo5692@126.com>
2022-04-23 20:48:26 +0800

    修复部分项目data解析失败

--- src/Task/Crontab.php
+++ src/Task/Crontab.php
@@ -31,11 +31,7 @@ class Crontab implements TaskInterface
 
     public function run(int $taskId, int $workerIndex)
     {
-        $data = $this->args;
-        if (!is_array($data)) {
-            trace(__METHOD__ . "仅支持数组传参, data:" . var_export($data, true), 'error');
-            return;
-        }
+        $data = is_array($this->args) ? $this->args : [];
 
         foreach ($data as $k => $v) {
             // 解析指定函数
