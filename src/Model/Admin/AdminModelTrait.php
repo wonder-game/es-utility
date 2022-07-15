0ef4d12b0b2b176a000ba7a55d32de3a31f87cd2
linkunyuan <171004297@qq.com>
2022-07-15 19:04:01 +0800

    暂存gameids为gameid

--- src/Model/Admin/AdminModelTrait.php
+++ src/Model/Admin/AdminModelTrait.php
@@ -32,20 +32,15 @@ trait AdminModelTrait
 	{
 		$array = is_array($extension) ? $extension : json_decode($extension, true);
 
-		if (isset($array['gameids']) && is_string($array['gameids'])) {
-			$array['gameids'] = explode(',', $array['gameids']);
-		}
+        // package、gameid、adid固定返回数组
+        foreach (['gameid', 'pkgbnd', 'adid'] as $col) {
+            $colValue = $array[$col] ?? [];
+            is_scalar($colValue) && $colValue = explode(',', $colValue);
+            $array[$col] = $colValue;
+        }
+        // gameid转整型
+        $array['gameids'] = array_map('intval', $array['gameids']);
 
-		// 如果是超级管理员可能是空数组或字符串
-		$array['gameids'] = array_map('intval', $array['gameids'] ?? []);
-		// package和gameid固定返回数组，gameid转整型
-		foreach (['gameids', 'pkgbnd'] as $col) {
-			$colValue = $array[$col] ?? [];
-			if (is_string($colValue)) {
-				$colValue = explode(',', $colValue);
-			}
-			$array[$col] = $colValue;
-		}
 		// 强类型限制, 游戏id有0
 		$array['gid'] = ! empty($array['gid']) ? intval($array['gid']) : '';
 
