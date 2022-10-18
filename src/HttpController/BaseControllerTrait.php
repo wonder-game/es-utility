47bbf907d368d9f8edf0a6a9cb224ea7c874f7fb
linkunyuan <171004297@qq.com>
2022-10-18 14:47:00 +0800

    _isRsa日志增加$data

--- src/HttpController/BaseControllerTrait.php
+++ src/HttpController/BaseControllerTrait.php
@@ -31,16 +31,16 @@ trait BaseControllerTrait
      */
     protected $input = [];
 
-	private $langsConstants = [];
+    private $langsConstants = [];
 
     protected $actionNotFoundPrefix = '_';
 
-	public function __construct()
-	{
-		parent::__construct();
+    public function __construct()
+    {
+        parent::__construct();
 
-		$this->setLanguageConstants();
-	}
+        $this->setLanguageConstants();
+    }
 
     protected function onRequest(?string $action): ?bool
     {
@@ -60,40 +60,40 @@ trait BaseControllerTrait
         $this->input = array_merge($this->get, $this->post);
     }
 
-	protected function setLanguageConstants()
-	{
-		$dictionary = config('CLASS_DICTIONARY');
-		if ( ! $dictionary || ! class_exists($dictionary)) {
-			$appLanguage = '\\App\\Common\\Languages\\Dictionary';
-			$dictionary = class_exists($appLanguage) ? $appLanguage : Dictionary::class;
-		}
-		$objClass = new \ReflectionClass($dictionary);
-		$this->langsConstants = $objClass->getConstants();
-	}
+    protected function setLanguageConstants()
+    {
+        $dictionary = config('CLASS_DICTIONARY');
+        if ( ! $dictionary || ! class_exists($dictionary)) {
+            $appLanguage = '\\App\\Common\\Languages\\Dictionary';
+            $dictionary = class_exists($appLanguage) ? $appLanguage : Dictionary::class;
+        }
+        $objClass = new \ReflectionClass($dictionary);
+        $this->langsConstants = $objClass->getConstants();
+    }
 
-	protected function getLanguageConstants()
-	{
-		return $this->langsConstants;
-	}
+    protected function getLanguageConstants()
+    {
+        return $this->langsConstants;
+    }
 
-	protected function _isRsa($input = [], $header = [], $category = 'pay')
-	{
-		// 则要求JWT要符合规则
-		$data = verify_token($input, $header, 'operid');
+    protected function _isRsa($input = [], $header = [], $category = 'pay')
+    {
+        // 则要求JWT要符合规则
+        $data = verify_token($input, $header, 'operid');
 
-		// 如果不是rsa加密数据并且非本地开发环境
-		if (empty($input['envkeydata']) && ! empty($data['INVERTOKEN']) && ! is_env('dev')) {
-			trace('密文有误:' . var_export($input, true), 'error', $category);
-			return false;
-		}
+        // 如果不是rsa加密数据并且非本地开发环境
+        if (empty($input['envkeydata']) && ! empty($data['INVERTOKEN']) && ! is_env('dev')) {
+            trace('密文有误:' . var_export($input, true) . var_export($data, true), 'error', $category);
+            return false;
+        }
 
-		unset($data['token']);
+        unset($data['token']);
 
-		return $data;
-	}
+        return $data;
+    }
 
-	protected function onException(\Throwable $throwable): void
-	{
+    protected function onException(\Throwable $throwable): void
+    {
         if ($throwable instanceof HttpParamException) {
             $message = $throwable->getMessage();
         } else {
@@ -101,8 +101,8 @@ trait BaseControllerTrait
             // 交给异常处理器
             \EasySwoole\EasySwoole\Trigger::getInstance()->throwable($throwable);
         }
-		$this->error($throwable->getCode() ?: Code::CODE_INTERNAL_SERVER_ERROR, $message);
-	}
+        $this->error($throwable->getCode() ?: Code::CODE_INTERNAL_SERVER_ERROR, $message);
+    }
 
     protected function success($result = null, $msg = null)
     {
@@ -115,53 +115,53 @@ trait BaseControllerTrait
         return false;
     }
 
-	protected function writeJson($statusCode = 200, $result = null, $msg = null)
-	{
-		if ( ! $this->response()->isEndResponse()) {
+    protected function writeJson($statusCode = 200, $result = null, $msg = null)
+    {
+        if ( ! $this->response()->isEndResponse()) {
 
-			if (is_null($msg)) {
-				$msg = Code::getReasonPhrase($statusCode);
-			} elseif ($msg && in_array($msg, $this->langsConstants)) {
-				$msg = lang($msg);
-			}
+            if (is_null($msg)) {
+                $msg = Code::getReasonPhrase($statusCode);
+            } elseif ($msg && in_array($msg, $this->langsConstants)) {
+                $msg = lang($msg);
+            }
 
-			$data = [
-				'code' => $statusCode,
-				'result' => $result,
-				'msg' => $msg ?? ''
-			];
-			$this->response()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
-			$this->response()->withHeader('Content-type', 'application/json;charset=utf-8');
-			// 浏览器对axios隐藏了http错误码和异常信息，如果程序出错，通过业务状态码告诉客户端
-			$this->response()->withStatus(Code::CODE_OK);
-			return true;
-		} else {
-			return false;
-		}
-	}
+            $data = [
+                'code' => $statusCode,
+                'result' => $result,
+                'msg' => $msg ?? ''
+            ];
+            $this->response()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+            $this->response()->withHeader('Content-type', 'application/json;charset=utf-8');
+            // 浏览器对axios隐藏了http错误码和异常信息，如果程序出错，通过业务状态码告诉客户端
+            $this->response()->withStatus(Code::CODE_OK);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	protected function writeUpload($url, $code = 200, $msg = '')
-	{
-		if ( ! $this->response()->isEndResponse()) {
+    protected function writeUpload($url, $code = 200, $msg = '')
+    {
+        if ( ! $this->response()->isEndResponse()) {
 
-			$data = [
-				'code' => $code,
-				'url' => $url,
-				'msg' => $msg
-			];
-			$this->response()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
-			$this->response()->withHeader('Content-type', 'application/json;charset=utf-8');
-			$this->response()->withStatus(Code::CODE_OK);
-			return true;
-		} else {
-			return false;
-		}
-	}
+            $data = [
+                'code' => $code,
+                'url' => $url,
+                'msg' => $msg
+            ];
+            $this->response()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+            $this->response()->withHeader('Content-type', 'application/json;charset=utf-8');
+            $this->response()->withStatus(Code::CODE_OK);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	protected function isMethod($method)
-	{
-		return strtoupper($this->request()->getMethod()) === strtoupper($method);
-	}
+    protected function isMethod($method)
+    {
+        return strtoupper($this->request()->getMethod()) === strtoupper($method);
+    }
 
     protected function isHttpGet()
     {
@@ -173,11 +173,11 @@ trait BaseControllerTrait
         return $this->isMethod('POST');
     }
 
-	protected function getStaticClassName()
-	{
-		$array = explode('\\', static::class);
-		return end($array);
-	}
+    protected function getStaticClassName()
+    {
+        $array = explode('\\', static::class);
+        return end($array);
+    }
 
     protected function actionNotFoundName()
     {
