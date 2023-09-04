0feb05e7d18168eda4bed82ad8c55bcea81946b8
lamson <171004297@qq.com>
2023-09-05 04:23:50 +0800

    feat: 修改HttpTracker配置

--- src/HttpTracker/SaveHandler.php
+++ src/HttpTracker/SaveHandler.php
@@ -9,38 +9,38 @@ use EasySwoole\Tracker\SaveHandlerInterface;
 
 class SaveHandler implements SaveHandlerInterface
 {
-	protected $config = [
-		'queue' => 'Report-Tracker',
-		'redis-name' => 'report',
-	];
-	
-	public function __construct(array $cfg = [])
-	{
-		$cfg && $this->config = array_merge($this->config, $cfg);
-	}
-	
-	/**
-	 * @param Point|null $point
-	 * @param array|null $globalArg
-	 * @return bool
-	 */
-	function save(?Point $point, ?array $globalArg = []): bool
-	{
-		if ($array = Point::toArray($point)) {
-			try {
-				RedisPool::invoke(function (Redis $redis) use ($array, $globalArg) {
-					foreach ($array as $value) {
-						$redis->lPush(
-							$this->config['queue'],
-							json_encode(array_merge($value, $globalArg ?? []), JSON_UNESCAPED_UNICODE)
-						);
-					}
-				}, $this->config['redis-name']);
-			} catch (\Exception | \Throwable $e) {
-				trace($e->getMessage(), 'error');
-				return false;
-			}
-		}
-		return true;
-	}
+    protected $config = [
+        'queue' => 'Report:Origin-HttpTracker',
+        'redis-name' => 'log',
+    ];
+
+    public function __construct(array $cfg = [])
+    {
+        $cfg && $this->config = array_merge($this->config, $cfg);
+    }
+
+    /**
+     * @param Point|null $point
+     * @param array|null $globalArg
+     * @return bool
+     */
+    function save(?Point $point, ?array $globalArg = []): bool
+    {
+        if ($array = Point::toArray($point)) {
+            try {
+                RedisPool::invoke(function (Redis $redis) use ($array, $globalArg) {
+                    foreach ($array as $value) {
+                        $redis->lPush(
+                            $this->config['queue'],
+                            json_encode(array_merge($value, $globalArg ?? []), JSON_UNESCAPED_UNICODE)
+                        );
+                    }
+                }, $this->config['redis-name']);
+            } catch (\Exception|\Throwable $e) {
+                trace($e->getMessage(), 'error');
+                return false;
+            }
+        }
+        return true;
+    }
 }
