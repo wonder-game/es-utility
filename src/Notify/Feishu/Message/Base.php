53621e9f470effbaf322a64d68bab4dc456abbd0
lamson <171004297@qq.com>
2024-06-03 20:12:43 +0800

    feat: 飞书支持发图片

--- src/Notify/Feishu/Message/Base.php
+++ src/Notify/Feishu/Message/Base.php
@@ -2,6 +2,7 @@
 
 namespace WonderGame\EsUtility\Notify\Feishu\Message;
 
+use EasySwoole\HttpClient\HttpClient;
 use EasySwoole\Spl\SplBean;
 use WonderGame\EsUtility\Notify\Interfaces\MessageInterface;
 
@@ -51,7 +52,7 @@ abstract class Base extends SplBean implements MessageInterface
             ];
         } else if (is_array($this->isAtAll)) {
             foreach ($this->atUserID as $key => $value) {
-                if (! in_array($key, $this->isAtAll)) continue;
+                if ( ! in_array($key, $this->isAtAll)) continue;
                 $at[] = [
                     'tag' => 'at',
                     'user_id' => $value['id'],
@@ -70,5 +71,75 @@ abstract class Base extends SplBean implements MessageInterface
                 '服务器：' . config('SERVNAME'),
                 '时间：' . date('Y年m月d日 H:i:s')
             ]);
+    }
+
+    public function getImageKey($img, $pool = 'admin')
+    {
+        $tenant_access_token = $this->tenantAccessToken($pool);
+        $headers = [
+            'Content-Type' => HttpClient::CONTENT_TYPE_FORM_DATA,
+            'Authorization' => "Bearer {$tenant_access_token}",
+        ];
+        $sendParams = [
+            'image_type' => 'message',
+            'image' => curl_file_create($img),
+        ];
+
+        $response = $this->postRequest('https://open.feishu.cn/open-apis/im/v1/images', $headers, $sendParams);
+        $result = json_decode($response, true);
+
+        if (isset($result['code']) && $result['code'] == 0) {
+            return $result['data']['image_key'];
+        } else {
+            return '';
+        }
+    }
+
+    public function tenantAccessToken($pool = 'admin')
+    {
+        $appId = config('ES_NOTIFY.feishu.appId');
+        $appSecret = config('ES_NOTIFY.feishu.appSecret');
+        $key = "tenant_access_token-{$appId}";
+        $Redis = defer_redis($pool);
+        $token = $Redis->get($key);
+        if ( ! empty($token)) {
+            return $token;
+        }
+
+        $sendParams = [
+            'app_id' => $appId,
+            'app_secret' => $appSecret,
+        ];
+        $HttpClient = new HttpClient('https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal');
+        $HttpClient->setClientSetting('keep_alive', true);
+        $response = $HttpClient->postJson(json_encode($sendParams));
+        $body = $response->getBody();
+        $result = json_decode($body, true);
+        if (isset($result['code']) && $result['code'] == 0) {
+            $Redis->setEx($key, $result['expire'] - 60, $result['tenant_access_token']);
+            return $result['tenant_access_token'];
+        }
+        return '';
+    }
+
+    protected function postRequest($urlString, $customerHeader, $body)
+    {
+        $url = $urlString;
+        $con = curl_init($url);
+        // 设置连接超时时间为5秒
+        curl_setopt($con, CURLOPT_CONNECTTIMEOUT, 5);
+        // 设置读取超时时间为10秒
+        curl_setopt($con, CURLOPT_TIMEOUT, 10);
+        curl_setopt($con, CURLOPT_POST, true);
+        curl_setopt($con, CURLOPT_POSTFIELDS, $body);
+        $headerArray = [];
+        foreach ($customerHeader as $key => $value) {
+            $headerArray[] = "$key: $value";
+        }
+        curl_setopt($con, CURLOPT_HTTPHEADER, $headerArray);
+        curl_setopt($con, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($con);
+        curl_close($con);
+        return $response;
     }
 }
